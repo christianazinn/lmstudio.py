fe2bf71b1066090608bbbfed094977dbf9d85165
Christian Zhou-Zheng <christianzhouzheng@gmail.com>
2024-09-19 15:32:36 -0400

    brought back async code in preparation to split

---
+++ src/lmstudio_sdk/async_temp/AsyncDiagnosticsNamespace.py
@@ -0,0 +1,40 @@
+from typing import Literal, Callable, TypedDict
+
+
+class DiagnosticsLogEventData(TypedDict):
+    type: Literal["llm.prediction.input"]
+    modelPath: str
+    modelIdentifier: str
+    input: str
+
+
+class DiagnosticsLogEvent(TypedDict):
+    timestamp: int
+    data: DiagnosticsLogEventData
+
+
+class DiagnosticsNamespace:
+    def __init__(self, port):
+        self.__port = port  # Type not specified in TypeScript
+
+    async def connect(self) -> None:
+        await self.__port.connect()
+
+    async def close(self) -> None:
+        await self.__port.close()
+
+    # TODO make me work
+    def unstable_stream_logs(self, listener: Callable[[DiagnosticsLogEvent], None]) -> Callable[[], None]:
+        """
+        Register a callback to receive log events. Return a function to stop receiving log events.
+
+        This method is in alpha. Do not use this method in production yet.
+        :alpha:
+        """
+
+        # TODO implement
+        def unsubscribe() -> None:
+            # Logic to stop receiving log events
+            pass
+
+        return unsubscribe
---
+++ src/lmstudio_sdk/async_temp/AsyncLLMDynamicHandle.py
@@ -0,0 +1,365 @@
+from typing import List, Callable, Optional, Dict, Union, Tuple
+
+from ...lms_dataclasses import (
+    convert_dict_to_kv_config,
+    find_key_in_kv_config,
+    KVConfig,
+    KVConfigLayerName,
+    KVConfigStack,
+    KVConfigStackLayer,
+    LLMApplyPromptTemplateOpts,
+    LLMCompletionContextInput,
+    LLMContext,
+    LLMConversationContextInput,
+    LLMPredictionConfig,
+    LLMPredictionExtraOpts,
+    LLMPredictionOpts,
+    LLMPredictionStats,
+    ModelDescriptor,
+    ModelSpecifier,
+    OngoingPrediction,
+)
+from ...typescript_ported import BufferedEvent
+from .DynamicHandle import DynamicHandle
+
+
+def number_to_checkbox_numeric(
+    value: Optional[float], unchecked_value: float, value_when_unchecked: float
+) -> Optional[Dict[str, Union[bool, float]]]:
+    if value is None:
+        return None
+    if value == unchecked_value:
+        return {"checked": False, "value": value_when_unchecked}
+    if value != unchecked_value:
+        return {"checked": True, "value": value}
+
+
+def prediction_config_to_kv_config(prediction_config: LLMPredictionConfig | None) -> KVConfig:
+    fields = []
+    if prediction_config is not None:
+        # HACK i hate this
+        for default_key in ["temperature", "context_overflow_policy", "stop_strings", "structured", "top_k_sampling"]:
+            if default_key in prediction_config:
+                fields.append({"key": default_key, "value": prediction_config[default_key]})
+        if "max_predicted_tokens" in prediction_config:
+            fields.append(
+                {
+                    "key": "max_predicted_tokens",
+                    "value": number_to_checkbox_numeric(prediction_config["max_predicted_tokens"], -1, 1),
+                }
+            )
+        if "repeat_penalty" in prediction_config:
+            fields.append(
+                {
+                    "key": "repeat_penalty",
+                    "value": number_to_checkbox_numeric(prediction_config["repeat_penalty"], 1, 1),
+                }
+            )
+        if "min_p_sampling" in prediction_config:
+            fields.append(
+                {
+                    "key": "min_p_sampling",
+                    "value": number_to_checkbox_numeric(prediction_config["min_p_sampling"], 0, 0.05),
+                }
+            )
+        if "top_p_sampling" in prediction_config:
+            fields.append(
+                {
+                    "key": "top_p_sampling",
+                    "value": number_to_checkbox_numeric(prediction_config["top_p_sampling"], 1, 0.95),
+                }
+            )
+        if "cpu_threads" in prediction_config:
+            fields.append({"key": "llama.cpu_threads", "value": prediction_config["cpu_threads"]})
+    return {"fields": fields}
+
+
+def split_opts(opts: LLMPredictionOpts) -> Tuple[LLMPredictionConfig, LLMPredictionExtraOpts]:
+    extra_opts: LLMPredictionExtraOpts = {}
+    for key in ["on_prompt_processing_progress", "on_first_token"]:
+        if key in opts:
+            extra_opts[key] = opts[key]
+            del opts[key]
+    return opts, extra_opts
+
+
+class LLMDynamicHandle(DynamicHandle):
+    """
+    This represents a set of requirements for a model. It is not tied to a specific model, but rather
+    to a set of requirements that a model must satisfy.
+
+    For example, if you got the model via `client.llm.get("my-identifier")`, you will get a
+    `LLMModel` for the model with the identifier `my-identifier`. If the model is unloaded, and
+    another model is loaded with the same identifier, using the same `LLMModel` will use the new
+    model.
+
+    :public:
+    """
+
+    __internal_kv_config_stack = KVConfigStack(layers=[])
+
+    async def __predict_internal(
+        self,
+        modelSpecifier: ModelSpecifier,
+        context: LLMContext,
+        predictionConfigStack: KVConfigStack,
+        cancel_event: BufferedEvent,
+        extraOpts: LLMPredictionExtraOpts,
+        on_fragment: Callable[[str], None],
+        on_finished: Callable[[LLMPredictionStats, ModelDescriptor, KVConfig, KVConfig], None],
+        on_error: Callable[[Exception], None],
+    ):
+        finished = False
+
+        async def handle_fragments(message: dict):
+            message_type = message.get("type", "")
+            if message_type == "fragment":
+                on_fragment(message.get("fragment", ""))
+                if "on_first_token" in extraOpts:
+                    on_first_token = extraOpts.get("on_first_token")
+                    if on_first_token is not None:
+                        on_first_token()
+            elif message_type == "promptProcessingProgress":
+                if "on_prompt_processing_progress" in extraOpts:
+                    on_prompt_processing_progress = extraOpts.get("on_prompt_processing_progress")
+                    if on_prompt_processing_progress is not None:
+                        on_prompt_processing_progress(message.get("progress", 0.0))
+            elif message_type == "success":
+                nonlocal finished
+                finished = True
+                on_finished(
+                    message.get("stats", {}),
+                    message.get("descriptor", {}),
+                    message.get("loadConfig", {}),
+                    message.get("predictionConfig", {}),
+                )
+            # FIXME this probably doesn't work
+            elif message_type == "error":
+                on_error(Exception(message.get("message", "Unknown error")))
+
+        channel_id = await self.port.create_channel(
+            "predict",
+            {
+                "modelSpecifier": modelSpecifier,
+                "context": context,
+                "predictionConfigStack": predictionConfigStack,
+            },
+            handle_fragments,
+        )
+
+        def cancel_send():
+            if not finished:
+                self.port.send_channel_message(channel_id, {"type": "cancel"})
+
+        cancel_event.subscribeOnce(cancel_send)
+
+    async def complete(self, prompt: LLMCompletionContextInput, opts: LLMPredictionOpts) -> OngoingPrediction:
+        """
+        Use the loaded model to predict text.
+
+        This method returns an {@link OngoingPrediction} object. An ongoing prediction can be used as a
+        promise (if you only care about the final result) or as an async iterable (if you want to
+        stream the results as they are being generated).
+
+        Example usage as a promise (Resolves to a {@link PredictionResult}):
+
+        ```typescript
+        const result = await model.complete("When will The Winds of Winter be released?");
+        console.log(result.content);
+        ```
+
+        Or
+
+        ```typescript
+        model.complete("When will The Winds of Winter be released?")
+         .then(result => console.log(result.content))
+         .catch(error => console.error(error));
+        ```
+
+        Example usage as an async iterable (streaming):
+
+        ```typescript
+        for await (const fragment of model.complete("When will The Winds of Winter be released?")) {
+          process.stdout.write(fragment);
+        }
+        ```
+
+        If you wish to stream the result, but also getting the final prediction results (for example,
+        you wish to get the prediction stats), you can use the following pattern:
+
+        ```typescript
+        const prediction = model.complete("When will The Winds of Winter be released?");
+        for await (const fragment of prediction) {
+          process.stdout.write(fragment);
+        }
+        const result = await prediction;
+        console.log(result.stats);
+        ```
+
+        :param prompt: The prompt to use for prediction.
+        :param opts: Options for the prediction.
+        """
+        config, extra_opts = split_opts(opts)
+
+        cancel_event, emit_cancel_event = BufferedEvent.create()
+        ongoing_prediction, finished, failed, push = OngoingPrediction.create(emit_cancel_event)
+
+        config["stop_strings"] = []
+        prediction_layers = self.__internal_kv_config_stack.get("layers", [])
+        prediction_layers.append(
+            {"layer_name": KVConfigLayerName.API_OVERRIDE, "config": prediction_config_to_kv_config(config)}
+        )
+        prediction_layers.append(
+            {
+                "layer_name": KVConfigLayerName.COMPLETE_MODE_FORMATTING,
+                "config": convert_dict_to_kv_config(
+                    {
+                        "promptTemplate": {
+                            "type": "jinja",
+                            "jinjaPromptTemplate": {
+                                "bosToken": "",
+                                "eosToken": "",
+                                "template": "{% for message in messages %}{{ message['content'] }}{% endfor %}",
+                            },
+                            "stop_strings": [],
+                        }
+                    }
+                ),
+            }
+        )
+
+        await self.__predict_internal(
+            self.specifier,
+            self.__resolve_completion_context(prompt),
+            {"layers": prediction_layers},
+            cancel_event,
+            extra_opts,
+            lambda fragment: push(fragment),
+            lambda stats, model_info, load_model_config, prediction_config: finished(
+                stats, model_info, load_model_config, prediction_config
+            ),
+            lambda error: failed(error),
+        )
+        return ongoing_prediction
+
+    def __resolve_completion_context(self, contextInput: LLMCompletionContextInput) -> LLMContext:
+        return {"history": [{"role": "user", "content": [{"type": "text", "text": contextInput}]}]}
+
+    async def respond(self, history: LLMConversationContextInput, opts: LLMPredictionOpts) -> OngoingPrediction:
+        """
+        Use the loaded model to generate a response based on the given history.
+
+        This method returns an {@link OngoingPrediction} object. An ongoing prediction can be used as a
+        promise (if you only care about the final result) or as an async iterable (if you want to
+        stream the results as they are being generated).
+
+        Example usage as a promise (Resolves to a {@link PredictionResult}):
+
+        ```typescript
+        const history = [{ role: 'user', content: "When will The Winds of Winter be released?" }];
+        const result = await model.respond(history);
+        console.log(result.content);
+        ```
+
+        Or
+
+        ```typescript
+        const history = [{ role: 'user', content: "When will The Winds of Winter be released?" }];
+        model.respond(history)
+         .then(result => console.log(result.content))
+         .catch(error => console.error(error));
+        ```
+
+        Example usage as an async iterable (streaming):
+
+        ```typescript
+        const history = [{ role: 'user', content: "When will The Winds of Winter be released?" }];
+        for await (const fragment of model.respond(history)) {
+          process.stdout.write(fragment);
+        }
+        ```
+
+        If you wish to stream the result, but also getting the final prediction results (for example,
+        you wish to get the prediction stats), you can use the following pattern:
+
+        ```typescript
+        const history = [{ role: 'user', content: "When will The Winds of Winter be released?" }];
+        const prediction = model.respond(history);
+        for await (const fragment of prediction) {
+          process.stdout.write(fragment);
+        }
+        const result = await prediction;
+        console.log(result.stats);
+        ```
+
+        :param history: The LLMChatHistory array to use for generating a response.
+        :param opts: Options for the prediction.
+        """
+        return await self.predict(self.__resolve_conversation_context(history), opts)
+
+    def __resolve_conversation_context(self, context_input: LLMConversationContextInput) -> LLMContext:
+        return {
+            "history": [
+                {"role": item.get("role", "user"), "content": [{"type": "text", "text": item.get("content", "")}]}
+                for item in context_input
+            ]
+        }
+
+    async def predict(self, context: LLMContext, opts: LLMPredictionOpts) -> OngoingPrediction:
+        """
+        :alpha:
+        """
+
+        config, extra_opts = split_opts(opts)
+
+        cancel_event, emit_cancel_event = BufferedEvent.create()
+        ongoing_prediction, finished, failed, push = OngoingPrediction.create(emit_cancel_event)
+
+        api_override_layer = KVConfigStackLayer(
+            layer_name=KVConfigLayerName.API_OVERRIDE, config=prediction_config_to_kv_config(config)
+        )
+        prediction_layers = self.__internal_kv_config_stack.get("layers", [])
+        prediction_layers.append(api_override_layer)
+
+        await self.__predict_internal(
+            self.specifier,
+            context,
+            KVConfigStack(layers=prediction_layers),
+            cancel_event,
+            extra_opts,
+            lambda fragment: push(fragment),
+            lambda stats, model_info, load_model_config, prediction_config: finished(
+                stats, model_info, load_model_config, prediction_config
+            ),
+            lambda error: failed(error),
+        )
+        return ongoing_prediction
+
+    async def unstable_get_context_length(self) -> int:
+        context_length = find_key_in_kv_config(await self.get_load_config(), "contextLength")
+        return context_length if context_length else -1
+
+    async def unstable_apply_prompt_template(
+        self, context: LLMContext, opts: LLMApplyPromptTemplateOpts | None = None
+    ) -> str:
+        return (
+            await self.port.call_rpc(
+                "applyPromptTemplate",
+                {
+                    "specifier": self.specifier,
+                    "context": context,
+                    "opts": opts,
+                    "predictionConfigStack": self.__internal_kv_config_stack,
+                },
+            )
+        ).get("formatted", "")
+
+    async def unstable_tokenize(self, input_string: str) -> List[int]:
+        return (await self.port.call_rpc("tokenize", {"specifier": self.specifier, "inputString": input_string})).get(
+            "tokens", [-1]
+        )
+
+    async def unstable_count_tokens(self, input_string: str) -> int:
+        return (
+            await self.port.call_rpc("countTokens", {"specifier": self.specifier, "inputString": input_string})
+        ).get("tokenCount", -1)
